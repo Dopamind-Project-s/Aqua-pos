6e550fb02f63e50bd98fca4df5c64128463ca1c8
Musab Al-Zoubi <126392782+MusabAlzoubi@users.noreply.github.com>
2026-02-28 02:38:33 +0300

    Add admin settings link to sidebar and include settings layout

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -51,6 +51,12 @@
                         <span class="hide-menu">Requests</span>
                     </a>
                 </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="{{ route('admin.settings.edit') }}" aria-expanded="false">
+                        <i class="ti ti-settings"></i>
+                        <span class="hide-menu">Settings</span>
+                    </a>
+                </li>
             </ul>
         </nav>
     </div>
