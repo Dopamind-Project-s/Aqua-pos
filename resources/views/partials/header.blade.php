298a8e78563aa8f3400238813d865abfd0aa5a78
Musab Al-Zoubi <126392782+MusabAlzoubi@users.noreply.github.com>
2026-02-27 23:54:14 +0300

    Refine responsive navbar sizing and product dropdown cards

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -34,7 +34,7 @@
         <div class="container-fluid position-relative p-0">
             <nav class="navbar navbar-expand-lg navbar-light bg-white px-4 px-lg-5 py-3 py-lg-0">
                 <a href="{{ url('/') }}" class="navbar-brand p-0">
-                    <h1 class="text-primary m-0"><i class="fas fa-star-of-life me-3"></i>AQUA POS</h1>
+                    <h1 class="navbar-brand__title text-primary m-0"><i class="fas fa-star-of-life me-2"></i>AQUA POS</h1>
                     <!-- <img src="img/logo.png" alt="Logo"> -->
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -42,7 +42,10 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto py-0">
-                        <a href="{{ route('home') }}" class="nav-item nav-link {{ request()->routeIs('home') ? 'active' : '' }}"><i class="fas fa-house me-2"></i><span data-i18n="nav.home">Home</span></a>
+                        <a href="{{ route('home') }}" class="nav-item nav-link nav-link--home {{ request()->routeIs('home') ? 'active' : '' }}" aria-label="Home">
+                            <i class="fas fa-house"></i>
+                            <span class="visually-hidden" data-i18n="nav.home">Home</span>
+                        </a>
                         <a href="{{ route('about') }}" class="nav-item nav-link {{ request()->routeIs('about') ? 'active' : '' }}"><i class="fas fa-circle-info me-2"></i><span data-i18n="nav.about">About</span></a>
                         <a href="{{ route('service') }}" class="nav-item nav-link {{ request()->routeIs('service') ? 'active' : '' }}"><i class="fas fa-concierge-bell me-2"></i><span data-i18n="nav.services">Services</span></a>
                         <div class="nav-item mega-menu" id="productsMegaMenu">
@@ -67,7 +70,7 @@
                                                     @foreach($menuCategory->products->take(6) as $menuProduct)
                                                         <a href="{{ route('products') }}" class="mega-menu__service">
                                                             <img class="mega-menu__service-thumb" src="{{ $menuProduct->image ? Storage::url($menuProduct->image) : asset('img/service-1.jpg') }}" alt="{{ $menuProduct->name }}">
-                                                            <div>
+                                                            <div class="mega-menu__service-content">
                                                                 <h5>{{ $menuProduct->name }}</h5>
                                                                 
                                                             </div>
@@ -81,7 +84,7 @@
                                                 <div class="mega-menu__services">
                                                     <a href="{{ route('products') }}" class="mega-menu__service">
                                                         <img class="mega-menu__service-thumb" src="{{ asset('img/service-1.jpg') }}" alt="Products">
-                                                        <div><h5>No active products yet</h5></div>
+                                                        <div class="mega-menu__service-content"><h5>No active products yet</h5></div>
                                                     </a>
                                                 </div>
                                             </article>
@@ -93,14 +96,14 @@
                                             <div class="mega-menu__services">
                                                 <a href="#" class="mega-menu__service">
                                                     <img src="{{ asset('img/service-1.jpg') }}" alt="Restaurant POS">
-                                                    <div>
+                                                    <div class="mega-menu__service-content">
                                                         <h5><span data-i18n="mega.restaurantPos">Restaurant POS</span></h5>
                                                         
                                                     </div>
                                                 </a>
                                                 <a href="#" class="mega-menu__service">
                                                     <img src="{{ asset('img/service-2.jpg') }}" alt="QR Ordering">
-                                                    <div>
+                                                    <div class="mega-menu__service-content">
                                                         <h5><span data-i18n="mega.qrOrdering">QR Ordering</span></h5>
                                                         
                                                     </div>
@@ -114,14 +117,14 @@
                                             <div class="mega-menu__services">
                                                 <a href="#" class="mega-menu__service">
                                                     <img src="{{ asset('img/service-3.jpg') }}" alt="Inventory Hub">
-                                                    <div>
+                                                    <div class="mega-menu__service-content">
                                                         <h5><span data-i18n="mega.inventoryHub">Inventory Hub</span></h5>
                                                         
                                                     </div>
                                                 </a>
                                                 <a href="#" class="mega-menu__service">
                                                     <img src="{{ asset('img/service-4.jpg') }}" alt="Loyalty CRM">
-                                                    <div>
+                                                    <div class="mega-menu__service-content">
                                                         <h5><span data-i18n="mega.loyaltyCrm">Loyalty CRM</span></h5>
                                                         
                                                     </div>
@@ -135,14 +138,14 @@
                                             <div class="mega-menu__services">
                                                 <a href="#" class="mega-menu__service">
                                                     <img src="{{ asset('img/service-5.jpg') }}" alt="Property PMS">
-                                                    <div>
+                                                    <div class="mega-menu__service-content">
                                                         <h5><span data-i18n="mega.propertyPms">Property PMS</span></h5>
                                                         
                                                     </div>
                                                 </a>
                                                 <a href="#" class="mega-menu__service">
                                                     <img src="{{ asset('img/service-6.jpg') }}" alt="Spa & Wellness">
-                                                    <div>
+                                                    <div class="mega-menu__service-content">
                                                         <h5><span data-i18n="mega.spaWellness">Spa & Wellness</span></h5>
                                                         
                                                     </div>
@@ -156,14 +159,14 @@
                                             <div class="mega-menu__services">
                                                 <a href="#" class="mega-menu__service">
                                                     <img src="{{ asset('img/service-7.jpg') }}" alt="BI Analytics">
-                                                    <div>
+                                                    <div class="mega-menu__service-content">
                                                         <h5><span data-i18n="mega.biAnalytics">BI Analytics</span></h5>
                                                         
                                                     </div>
                                                 </a>
                                                 <a href="#" class="mega-menu__service">
                                                     <img src="{{ asset('img/service-8.jpg') }}" alt="API Integrations">
-                                                    <div>
+                                                    <div class="mega-menu__service-content">
                                                         <h5><span data-i18n="mega.apiIntegrations">API Integrations</span></h5>
                                                         
                                                     </div>
