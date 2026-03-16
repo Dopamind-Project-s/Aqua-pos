a35c37d1254225e1c3fbe8a78c533c47d06331a6
Musab Al-Zoubi <126392782+MusabAlzoubi@users.noreply.github.com>
2026-03-16 03:39:34 +0300

    Fix RTL language switch for hero and slider components

--- public/js/main.js
+++ public/js/main.js
@@ -26,37 +26,67 @@
     });
 
 
-    // Hero Header carousel
-    $(".header-carousel").owlCarousel({
-        animateOut: 'slideOutDown',
-        items: 1,
-        autoplay: true,
-        smartSpeed: 1000,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-    });
+    var isRtlLayout = function () {
+        return document.documentElement.getAttribute('dir') === 'rtl';
+    };
 
+    var refreshOwlCarousel = function (selector, options) {
+        var $carousel = $(selector);
+        if (!$carousel.length) {
+            return;
+        }
 
-    // International carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        items: 1,
-        smartSpeed: 1500,
-        dots: true,
-        loop: true,
-        margin: 25,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ]
-    });
+        $carousel.each(function () {
+            var $el = $(this);
+            if ($el.hasClass('owl-loaded')) {
+                $el.trigger('destroy.owl.carousel');
+                $el.removeClass('owl-loaded');
+                $el.find('.owl-stage-outer').children().unwrap();
+            }
 
+            $el.owlCarousel(Object.assign({}, options, { rtl: isRtlLayout() }));
+        });
+    };
+
+    window.aquaRefreshCarousels = function () {
+        refreshOwlCarousel('.header-carousel', {
+            animateOut: 'slideOutDown',
+            items: 1,
+            autoplay: true,
+            smartSpeed: 1000,
+            dots: false,
+            loop: true,
+            nav: true,
+            navText: [
+                '<i class="bi bi-arrow-left"></i>',
+                '<i class="bi bi-arrow-right"></i>'
+            ]
+        });
+
+        refreshOwlCarousel('.testimonial-carousel', {
+            autoplay: true,
+            smartSpeed: 1000,
+            center: true,
+            dots: true,
+            loop: true,
+            margin: 25,
+            nav: true,
+            navText: [
+                '<i class="bi bi-arrow-left"></i>',
+                '<i class="bi bi-arrow-right"></i>'
+            ],
+            responsiveClass: true,
+            responsive: {
+                0: { items: 1 },
+                576: { items: 1 },
+                768: { items: 1 },
+                992: { items: 1 },
+                1200: { items: 1 }
+            }
+        });
+    };
+
+    window.aquaRefreshCarousels();
 
     // Modal Video
     $(document).ready(function () {
@@ -76,41 +106,6 @@
     });
 
 
-    // testimonial carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        dots: true,
-        loop: true,
-        margin: 25,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:1
-            },
-            992:{
-                items:1
-            },
-            1200:{
-                items:1
-            }
-        }
-    });
-
-    
-    
    // Back to top button
    $(window).scroll(function () {
     if ($(this).scrollTop() > 300) {
@@ -675,6 +670,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
         localStorage.setItem(LANG_KEY, lang);
         setTheme(currentTheme);
+
+        if (typeof window.aquaRefreshCarousels === 'function') {
+            window.aquaRefreshCarousels();
+        }
+
+        document.dispatchEvent(new CustomEvent('aqua:language-changed', {
+            detail: { lang: lang }
+        }));
     };
 
     document.addEventListener('DOMContentLoaded', function () {
